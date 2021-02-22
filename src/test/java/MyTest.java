0ab562b80bb29a52fe7dfcaae7c070169055f04c
agwanyanjaba <bonkewycliffe@yahoo.com>
2021-02-22 07:21:16 +0300

    Sample Azure DevOps CI/CD java project

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,14 @@
+import com.wybosoft.wybosoftapp.DemoClass;
+import org.junit.Test;
+
+public class MyTest {
+    @Test
+    public void test_method_1() {
+        DemoClass d = new DemoClass();
+        d.DoSomething(true);
+    }
+
+    @Test
+    public void test_method_2() {
+    }
+}
